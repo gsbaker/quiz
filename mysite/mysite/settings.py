666294f6ff9bf24c06d00804730e9cf32d4378e7
George Baker <gsbaker2000@icloud.com>
2020-09-27 18:00:19 +0000

    Added STATIC_ROOT

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -130,4 +130,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASEDIR, 'static')
 STATIC_URL = '/static/'
